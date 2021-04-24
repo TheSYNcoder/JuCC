5b509dd40f0c5c4aa6c8f40441cf1e6414ddf345
noob77777 <abhishekpal7@outlook.com>
2021-04-25 04:44:05 +0530

    Fixed: lint

--- test/grammar/grammar_test.cpp
+++ test/grammar/grammar_test.cpp
@@ -4,54 +4,54 @@
 
 using jucc::grammar::Parser;
 
-TEST(grammar, parser_test_0) {
+TEST(grammar, parserTest0) {
   Parser parser = Parser("../test/grammar/grammar_test_0.g");
   ASSERT_EQ(true, parser.Parse());
 }
 
-TEST(grammar, parser_test_1) {
+TEST(grammar, parserTest1) {
   Parser parser = Parser("../test/grammar/grammar_test_1.g");
   ASSERT_EQ(false, parser.Parse());
   ASSERT_EQ("grammar parsing error: invalid token %terminals", parser.GetError());
 }
 
-TEST(grammar, parser_test_2) {
+TEST(grammar, parserTest2) {
   Parser parser = Parser("../test/grammar/grammar_test_2.g");
   ASSERT_EQ(false, parser.Parse());
   ASSERT_EQ("grammar parsing error: invalid token %non_terminals", parser.GetError());
 }
 
-TEST(grammar, parser_test_3) {
+TEST(grammar, parserTest3) {
   Parser parser = Parser("../test/grammar/grammar_test_3.g");
   ASSERT_EQ(false, parser.Parse());
   ASSERT_EQ("grammar parsing error: invalid token %rules", parser.GetError());
 }
 
-TEST(grammar, parser_test_4) {
+TEST(grammar, parserTest4) {
   Parser parser = Parser("../test/grammar/grammar_test_4.g");
   ASSERT_EQ(false, parser.Parse());
   ASSERT_EQ("grammar parsing error: invalid token %", parser.GetError());
 }
 
-TEST(grammar, parser_test_5) {
+TEST(grammar, parserTest5) {
   Parser parser = Parser("../test/grammar/grammar_test_5.g");
   ASSERT_EQ(false, parser.Parse());
   ASSERT_EQ("grammar parsing error: invalid token outside block: bruh", parser.GetError());
 }
 
-TEST(grammar, parser_test_6) {
+TEST(grammar, parserTest6) {
   Parser parser = Parser("../test/grammar/grammar_test_6.g");
   ASSERT_EQ(false, parser.Parse());
   ASSERT_EQ("grammar parsing error: EPSILON is reserved", parser.GetError());
 }
 
-TEST(grammar, parser_test_7) {
+TEST(grammar, parserTest7) {
   Parser parser = Parser("../test/grammar/grammar_test_7.g");
   ASSERT_EQ(false, parser.Parse());
   ASSERT_EQ("grammar parsing error: EPSILON is reserved", parser.GetError());
 }
 
-TEST(grammar, parser_test_na) {
+TEST(grammar, parserTestFile) {
   Parser parser = Parser("invalid_file_path");
   ASSERT_EQ(false, parser.Parse());
   ASSERT_EQ("grammar parsing error: file not found", parser.GetError());
