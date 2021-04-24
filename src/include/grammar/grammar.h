c6d094675d6673ec6cfcbfb648b1985ddbc4fbc9
Bisakh Mondal <bisakhmondal00@gmail.com>
2021-04-25 00:24:37 +0530

    Constructors, getters and setters

--- src/include/grammar/grammar.h
+++ src/include/grammar/grammar.h
@@ -1,5 +1,6 @@
 #ifndef JUCC_GRAMMAR_H
 #define JUCC_GRAMMAR_H
+
 #include <string>
 #include <vector>
 
@@ -9,12 +10,27 @@ const char *EPSILON = "epsilon";
 
 class Rule {
   std::vector<std::string> entities;
+
+ public:
+  Rule() = default;
+  explicit Rule(std::vector<std::string> entities) : entities(std::move(entities)) {}
+  [[nodiscard]] const std::vector<std::string> &getEntities() const { return entities; }
+  void setEntities(const std::vector<std::string> &entities) { Rule::entities = entities; }
 };
 
 class Production {
   std::string parent;
   std::vector<Rule> rules;
+
+ public:
+  Production() = default;
+  [[nodiscard]] const std::string &getParent() const { return parent; }
+  [[nodiscard]] const std::vector<Rule> &getRules() const { return rules; }
+  void setParent(const std::string &parent) { Production::parent = parent; }
+  void setRules(const std::vector<Rule> &rules) { Production::rules = rules; }
 };
+
+typedef std::vector<Production> Productions;
 
 }  // namespace grammar
 }  // namespace jucc
