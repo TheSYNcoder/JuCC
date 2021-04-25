1e82b004b1660b68eaf7a0af011ad6477cbcb90e
Bisakh Mondal <bisakhmondal00@gmail.com>
2021-04-26 01:12:37 +0530

    more comments!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- src/include/utils/left_recursion.h
+++ src/include/utils/left_recursion.h
@@ -3,12 +3,36 @@
 #include "grammar/grammar.h"
 
 namespace jucc::utils {
+// Standard practice.
 const char DASH[] = "'";
 
+/**
+ * Removes  direct left Recursion.
+ * Example: for production
+ * E -> E + T
+ * E -> T
+ * E -> EPSILON
+ * @return a set of productions after removing left recursion.
+ * E'   -> +TE' | EPSILON
+ * E    -> TE' | EPSILON E'
+ */
 grammar::Productions RemoveDirectLeftRecursion(const grammar::Production & /*prod*/);
+
+/**
+ * Checks if a given production is recursive.
+ * @return boolean
+ */
 bool IsRecursive(const grammar::Production & /*prod*/);
 
-grammar::Productions RemoveIndirectLeftRecursions(const grammar::Productions &);
+/**
+ * TODO!! (If required)
+ * Removes Indirect Productions.
+ * A -> B
+ * B -> C
+ * C -> A | B | EPSILON
+ * @return a set of Productions.
+ */
+grammar::Productions RemoveIndirectLeftRecursions(const grammar::Productions & /*prod*/);
 
 }  // namespace jucc::utils
 #endif  // JUCC_LEFT_RECURSION_H
