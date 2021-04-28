314e87752fcac35cebc30da9727814ca2c4ccb1b
Bisakh Mondal <bisakhmondal00@gmail.com>
2021-04-28 22:59:52 +0530

    comments

--- src/include/utils/first_follow.h
+++ src/include/utils/first_follow.h
@@ -17,7 +17,7 @@ const char STRING_ENDMARKER[] = "$";
  * For each terminal and non terminal in the grammar compute if symbol is / derives to nullable
  * Terminals except EPSILON are defaulted to non nullable
  * @returns an unordered_map keyed by each symbol in the grammar of booleans
- * true if symbol is nullable, false otherwise
+ * true if symbol is nullable, false otherwise.
  */
 std::unordered_map<std::string, bool> CalcNullables(const grammar::Productions & /*augmented_grammar*/);
 
@@ -26,14 +26,14 @@ using SymbolsMap = std::unordered_map<std::string, std::vector<std::string>>;
 /**
  * For each non terminal in given set of productions computes Firsts, and in case of terminals
  * it returns the same.
- * @returns an unordered_map keyed by non terminals in the grammar of a vector of terminals
+ * @returns an unordered_map keyed by the each symbol in the grammar of a vector of terminals.
  */
 SymbolsMap CalcFirsts(const grammar::Productions & /*augmented_grammar*/,
                       const std::unordered_map<std::string, bool> & /*nullables*/);
 
 /**
  * For each non terminal in given set of productions computes Follows
- * @returns an unordered_map keyed by non terminals in the grammar of a vector of terminals
+ * @returns an unordered_map keyed by non terminals in the grammar of a vector of terminals.
  */
 SymbolsMap CalcFollows(const grammar::Productions & /*augmented_grammar*/, const SymbolsMap & /*firsts*/,
                        const std::unordered_map<std::string, bool> & /*nullables*/,
