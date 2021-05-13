d9fdeefde6563b452bcf858943adea05ac171ca8
Abhishek Pal <42897033+noob77777@users.noreply.github.com>
2021-05-13 11:35:00 +0530

    Add: benchmark (#44)
    * Fixed: codecov benchmark
    
    * Added: benchmark
    
    * Added: benchmark
    
    * Fixed: lint
    
    * Added: benchmark file
    
    * Update: README.md
    
    * Fixed: comments

--- benchmark/main/jucc_benchmark.cpp
+++ benchmark/main/jucc_benchmark.cpp
@@ -3,10 +3,104 @@
 
 // NOLINTNEXTLINE
 static void BenchmarkRadd(benchmark::State &state) {
-  // NOLINTNEXTLINE
-  for (auto _ : state) {
+  for (const auto &_ : state) {
     jucc::Radd(0, 1000000);
   }
 }
 
 BENCHMARK(BenchmarkRadd);
+
+// NOLINTNEXTLINE
+static void BenchmarkJuCC(benchmark::State &state) {
+  std::string file_grammar = "../benchmark/input/benchmark_grammar.g";
+  std::string file_input = "../benchmark/input/in1.cc";
+  for (const auto &_ : state) {
+    jucc::grammar::Parser grammar_parser = jucc::grammar::Parser(file_grammar.c_str());
+    grammar_parser.Parse();
+    jucc::grammar::Productions raw_productions = grammar_parser.GetProductions();
+    jucc::grammar::Productions productions = jucc::utils::RemoveAllPossibleAmbiguity(raw_productions);
+    auto nullables = jucc::utils::CalcNullables(productions);
+    auto firsts = jucc::utils::CalcFirsts(productions, nullables);
+    auto follows = jucc::utils::CalcFollows(productions, firsts, nullables, grammar_parser.GetStartSymbol());
+    auto terminals = grammar_parser.GetTerminals();
+    auto non_terminals = jucc::utils::GetAllNonTerminals(productions);
+    jucc::parser::ParsingTable parsing_table = jucc::parser::ParsingTable(terminals, non_terminals);
+    parsing_table.SetFirsts(firsts);
+    parsing_table.SetFollows(follows);
+    parsing_table.SetProductions(productions);
+    parsing_table.BuildTable();
+
+    std::ifstream ifs(file_input);
+    std::vector<std::string> input_tokens;
+    jucc::lexer::Lexer lexer = jucc::lexer::Lexer();
+    int token;
+    while ((token = lexer.GetToken(ifs)) != jucc::lexer::TOK_EOF) {
+      std::string ret_string = jucc::lexer::Lexer::GetTokenType(token);
+      if (ret_string == "ignore") {
+        continue;
+      }
+      input_tokens.emplace_back(ret_string);
+    }
+
+    jucc::parser::Parser parser = jucc::parser::Parser();
+    parser.SetInputString(input_tokens);
+    parser.SetStartSymbol(grammar_parser.GetStartSymbol());
+    parser.SetParsingTable(parsing_table);
+    while (!parser.IsComplete()) {
+      parser.ParseNextStep();
+    }
+    parser.BuildParseTree();
+  }
+}
+
+BENCHMARK(BenchmarkJuCC);
+
+/*
+ * Same as BenchmarkJuCC but without disk operations
+ */
+// NOLINTNEXTLINE
+static void BenchmarkJuCCCompute(benchmark::State &state) {
+  std::string file_grammar = "../benchmark/input/benchmark_grammar.g";
+  std::string file_input = "../benchmark/input/in1.cc";
+
+  jucc::grammar::Parser grammar_parser = jucc::grammar::Parser(file_grammar.c_str());
+  grammar_parser.Parse();
+
+  std::ifstream ifs(file_input);
+  std::vector<std::string> input_tokens;
+  jucc::lexer::Lexer lexer = jucc::lexer::Lexer();
+  int token;
+  while ((token = lexer.GetToken(ifs)) != jucc::lexer::TOK_EOF) {
+    std::string ret_string = jucc::lexer::Lexer::GetTokenType(token);
+    if (ret_string == "ignore") {
+      continue;
+    }
+    input_tokens.emplace_back(ret_string);
+  }
+
+  for (const auto &_ : state) {
+    jucc::grammar::Productions raw_productions = grammar_parser.GetProductions();
+    jucc::grammar::Productions productions = jucc::utils::RemoveAllPossibleAmbiguity(raw_productions);
+    auto nullables = jucc::utils::CalcNullables(productions);
+    auto firsts = jucc::utils::CalcFirsts(productions, nullables);
+    auto follows = jucc::utils::CalcFollows(productions, firsts, nullables, grammar_parser.GetStartSymbol());
+    auto terminals = grammar_parser.GetTerminals();
+    auto non_terminals = jucc::utils::GetAllNonTerminals(productions);
+    jucc::parser::ParsingTable parsing_table = jucc::parser::ParsingTable(terminals, non_terminals);
+    parsing_table.SetFirsts(firsts);
+    parsing_table.SetFollows(follows);
+    parsing_table.SetProductions(productions);
+    parsing_table.BuildTable();
+
+    jucc::parser::Parser parser = jucc::parser::Parser();
+    parser.SetInputString(input_tokens);
+    parser.SetStartSymbol(grammar_parser.GetStartSymbol());
+    parser.SetParsingTable(parsing_table);
+    while (!parser.IsComplete()) {
+      parser.ParseNextStep();
+    }
+    parser.BuildParseTree();
+  }
+}
+
+BENCHMARK(BenchmarkJuCCCompute);
